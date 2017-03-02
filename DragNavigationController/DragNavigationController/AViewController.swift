7fdad2da7cc73a15f39293fbd71033832d5a7173
monkeyRing <502086651@qq.com>
2017-03-02 14:55:47 +0800

    提交

--- DragNavigationController/DragNavigationController/AViewController.swift
+++ DragNavigationController/DragNavigationController/AViewController.swift
@@ -13,9 +13,9 @@ class AViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        dismiss(animated: true, completion: nil)
     }
 
 }
--- DragNavigationController/DragNavigationController/HLNavigationController.swift
+++ DragNavigationController/DragNavigationController/HLNavigationController.swift
@@ -8,28 +8,42 @@
 
 import UIKit
 
-func T_Window() -> UIWindow {return UIApplication.sharedApplication().keyWindow!}
+func T_Window() -> UIWindow { return UIApplication.shared.keyWindow! }
 
 func T_WindowView() -> UIView {
     
-    if let windowView = UIApplication.sharedApplication().keyWindow?.rootViewController?.view {
+    if var windowView = UIApplication.shared.keyWindow?.rootViewController?.view {
+        
+        let ctrl = UIApplication.shared.keyWindow?.rootViewController;
+        
+        while (true) {
+        
+            if ((ctrl?.presentedViewController) != nil) {
+                windowView = (ctrl?.presentedViewController?.view)!; break
+            } else {
+                break
+            }
+        }
+        
         return windowView
     }else {
-        return UIView()  //
+        
+        return UIView()
     }
 }
 
-func width() -> CGFloat { return UIScreen.mainScreen().bounds.size.width}
-func height() -> CGFloat { return UIScreen.mainScreen().bounds.size.height}
+func width() -> CGFloat { return UIScreen.main.bounds.size.width}
+func height() -> CGFloat { return UIScreen.main.bounds.size.height}
 let TOUCH_DISTANCE: CGFloat = 80
 
+// 拖拽返回的效果 3D效果/类似美团外卖 平移效果/雪球
 enum PopAnimationType {
-    case PopAnimationTypeFromeBehind , PopAnimationTypeliner
+    case popAnimationTypeFromeBehind , popAnimationTypeliner
 }
 
-class HLNaviViewController: UINavigationController, UINavigationControllerDelegate,UIGestureRecognizerDelegate{
+class HLNavigationController: UINavigationController, UINavigationControllerDelegate,UIGestureRecognizerDelegate{
     
-    internal var popAnimationType:PopAnimationType? // 拖拽返回的动画效果 3D效果/类似美团外卖 平移效果/雪球
+    internal var popAnimationType:PopAnimationType?
     internal var canDragBack:Bool?   // 是否能拖拽
     
     var snapArray: NSMutableArray?
@@ -39,7 +53,7 @@ class HLNaviViewController: UINavigationController, UINavigationControllerDelega
     var lastScreenShotImageView: UIImageView?
     var isMoving: Bool?
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         snapArray = NSMutableArray()
         canDragBack = true
@@ -61,25 +75,25 @@ class HLNaviViewController: UINavigationController, UINavigationControllerDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.interactivePopGestureRecognizer?.enabled = false
+        self.interactivePopGestureRecognizer?.isEnabled = false
         let panRecognizer = UIPanGestureRecognizer.init(target: self, action: #selector(didPanGestureRecognizer(_:)))
         panRecognizer.delegate = self
         self.view.addGestureRecognizer(panRecognizer)
     }
     
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         if snapArray?.count == 0 {
             if let captureImage = capture() {
-                self.snapArray?.addObject(captureImage)
+                self.snapArray?.add(captureImage)
             }
         }
     }
     
-    override func pushViewController(viewController: UIViewController, animated: Bool) {
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         
         if let captureImage = capture() {
-            self.snapArray?.addObject(captureImage)
+            self.snapArray?.add(captureImage)
         }
         
         if self.viewControllers.count == 1{
@@ -88,12 +102,12 @@ class HLNaviViewController: UINavigationController, UINavigationControllerDelega
         super.pushViewController(viewController, animated: animated)
     }
     
-    override func popViewControllerAnimated(animated: Bool) -> UIViewController? {
+    override func popViewController(animated: Bool) -> UIViewController? {
         self.snapArray?.removeLastObject()
-        return super.popViewControllerAnimated(animated)
+        return super.popViewController(animated: animated)
     }
     
-    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
         if (self.viewControllers.count <= 1 || self.canDragBack == false) {
             return false
         }
@@ -101,43 +115,44 @@ class HLNaviViewController: UINavigationController, UINavigationControllerDelega
     }
     
     
-    func didPanGestureRecognizer(pan: UIPanGestureRecognizer) {
+    func didPanGestureRecognizer(_ pan: UIPanGestureRecognizer) {
         
         if (self.viewControllers.count <= 1 || self.canDragBack == false){
             return
         }
         
-        let touchPoint = pan.locationInView(T_Window())
-        if pan.state == .Began {
+        let touchPoint = pan.location(in: T_Window())
+        if pan.state == .began {
             self.startPoint = touchPoint
             self.isMoving = true
             addSnapView()
-        } else if(pan.state == .Ended){
+        } else if(pan.state == .ended){
             if (CGFloat(touchPoint.x - self.startPoint!.x) > TOUCH_DISTANCE) {
-                UIView.animateWithDuration(0.3, animations: { 
+                UIView.animate(withDuration: 0.3, animations: { 
                         self.doMoveViewWithX(width())
                     }, completion: { finished in
                         self.isMoving = false
-                        self.popViewControllerAnimated(false)
+                        _ = self.popViewController(animated: false)
                         var frame = T_WindowView().frame
                         frame.origin.x = 0
                         T_WindowView().frame = frame
                 })
             }else{
-                UIView.animateWithDuration(0.3, animations: { 
+                UIView.animate(withDuration: 0.3, animations: { 
                         self.doMoveViewWithX(0)
                     }, completion: { finished in
                         self.isMoving = false
-                        self.backgroundView?.hidden = true
+                        self.backgroundView?.isHidden = true
                 })
             }
             return
-        }else if(pan.state == .Cancelled){
-            UIView.animateWithDuration(0.3, animations: { 
+        }else if(pan.state == .cancelled){
+            
+            UIView.animate(withDuration: 0.3, animations: { 
                     self.doMoveViewWithX(0)
                 }, completion: { finished in
                     self.isMoving = false
-                    self.backgroundView?.hidden = true
+                    self.backgroundView?.isHidden = true
             })
             return
         }
@@ -151,26 +166,26 @@ class HLNaviViewController: UINavigationController, UINavigationControllerDelega
     func addSnapView(){
         if self.backgroundView == nil {
             self.backgroundView = UIView.init(frame: self.view.bounds)
-            self.backgroundView?.backgroundColor = UIColor.blackColor()
+            self.backgroundView?.backgroundColor = UIColor.black
             T_WindowView().superview?.insertSubview(self.backgroundView!, belowSubview: T_WindowView())
-            self.blackMask = UIView.init(frame: CGRectMake(0, 0, width() , height()))
-            self.blackMask?.backgroundColor = UIColor.blackColor()
+            self.blackMask = UIView.init(frame: CGRect(x: 0, y: 0, width: width() , height: height()))
+            self.blackMask?.backgroundColor = UIColor.black
             self.backgroundView?.addSubview(self.blackMask!)
         }
         
-        self.backgroundView?.hidden = false
+        self.backgroundView?.isHidden = false
         if self.lastScreenShotImageView != nil {
             self.lastScreenShotImageView?.removeFromSuperview()
         }
         
         let lastScreenShot = self.snapArray?.lastObject as? UIImage
         self.lastScreenShotImageView = UIImageView(image: lastScreenShot)
-        self.lastScreenShotImageView?.frame = CGRectMake(0, 0, width(),height())
+        self.lastScreenShotImageView?.frame = CGRect(x: 0, y: 0, width: width(),height: height())
         self.backgroundView?.insertSubview(self.lastScreenShotImageView!, belowSubview: self.blackMask!)
     }
     
-    
-    func doMoveViewWithX(var x:CGFloat){
+    func doMoveViewWithX(_ x:CGFloat){
+        var x = x
         x = x > width() ? width():x
         x = x < 0 ? 0 : x
         var frame = T_WindowView().frame
@@ -181,15 +196,15 @@ class HLNaviViewController: UINavigationController, UINavigationControllerDelega
 //        animationLiner(x)  // 线性
     }
     
-    func animationFromBehind(x: CGFloat){
+    func animationFromBehind(_ x: CGFloat){
         let coeffiient = width() * 25
         let scale = (x/coeffiient) + 0.96
         let alpha = 0.4 - (x/800)
-        self.lastScreenShotImageView?.transform = CGAffineTransformMakeScale(scale, scale)
+        self.lastScreenShotImageView?.transform = CGAffineTransform(scaleX: scale, y: scale)
         self.blackMask?.alpha = alpha
     }
     
-    func animationLiner(x: CGFloat){
+    func animationLiner(_ x: CGFloat){
         let coefficient = x / 2
         var screenShotImageViewFrame = self.lastScreenShotImageView?.frame
         screenShotImageViewFrame?.origin.x = -width()/2 + coefficient
@@ -203,10 +218,10 @@ class HLNaviViewController: UINavigationController, UINavigationControllerDelega
      - returns: 截屏图片
      */
     func capture() -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(CGSizeMake(T_WindowView().frame.width, T_WindowView().frame.height), T_WindowView().opaque, 0.0)
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: T_WindowView().frame.width, height: T_WindowView().frame.height), T_WindowView().isOpaque, 0.0)
         
         if let currentCtx = UIGraphicsGetCurrentContext() {
-            T_WindowView().layer.renderInContext(currentCtx)
+            T_WindowView().layer.render(in: currentCtx)
         }
         
         let img = UIGraphicsGetImageFromCurrentImageContext()
